e8b7f4ac0874751bed9f33cd356275dabd007213
jackarmley <jackarmley@gmail.com>
2015-12-13 20:51:12 +0000

    page follow nav

--- src/public/js/frontend.js
+++ src/public/js/frontend.js
@@ -23,9 +23,39 @@ app.menuToggle = function() {
             this.innerHTML = triggerText.active;
         }
     });
-}
+};
+
+/**
+ * Follow nav
+ */
+app.followNav = function() {
+    var nav = document.querySelector('.page-header'),
+        navHeight = nav.clientHeight,
+        content = document.querySelector('.page-content');
+        scrollLevel = 0,
+        compactClass = 'page-header--compact';
+    console.log(content.style.paddingTop);
+    window.addEventListener('scroll', function() {
+        setInterval(
+            function() {
+                scrollLevel = window.scrollY;
+                if(scrollLevel > navHeight){
+                    nav.classList.add(compactClass);
+                    content.setAttribute('style', 'padding-top:' + navHeight + 'px');
+                }else{
+                    nav.classList.remove(compactClass);
+                    content.removeAttribute('style');
+                }
+            },
+            500
+        );
+    });
+};
 
 /**
  * Init
  */
 app.menuToggle();
+if(!Modernizr.touchevents) {
+    app.followNav();
+}
