b49917fa4ab8a6f87e4b5b4e0b8b481b645faf99
jackarmley <jackarmley@gmail.com>
2016-01-08 00:13:23 +0000

    counter in

--- src/public/js/frontend.js
+++ src/public/js/frontend.js
@@ -51,10 +51,51 @@ app.followNav = function() {
     });
 };
 
+app.counter = function(yr,m,d) {
+    // @src:
+    // http://www.javascriptkit.com/script/script2/count2.shtml
+    var current ="We've launched!"
+    var montharray = new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+
+        theyear=yr;themonth=m;theday=d
+        var elem = document.querySelector("#join-us .shard-heading");
+        var today=new Date()
+        var todayy=today.getYear()
+        if (todayy < 1000)
+        todayy+=1900
+        var todaym=today.getMonth()
+        var todayd=today.getDate()
+        var todayh=today.getHours()
+        var todaymin=today.getMinutes()
+        var todaysec=today.getSeconds()
+        var todaystring=montharray[todaym]+" "+todayd+", "+todayy+" "+todayh+":"+todaymin+":"+todaysec
+        futurestring=montharray[m-1]+" "+d+", "+yr
+        dd=Date.parse(futurestring)-Date.parse(todaystring)
+        dday=Math.floor(dd/(60*60*1000*24)*1)
+        dhour=Math.floor((dd%(60*60*1000*24))/(60*60*1000)*1)
+        dmin=Math.floor(((dd%(60*60*1000*24))%(60*60*1000))/(60*1000)*1)
+        dsec=Math.floor((((dd%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1)
+        if(dday==0&&dhour==0&&dmin==0&&dsec==1){
+        document.forms.count.count2.value=current
+        return
+        }
+        else
+            elem.innerHTML = "In " + dday+ " days, "+dhour+" hours, "+dmin+" minutes, and " + dsec + " seconds...";
+        //setTimeout("this(theyear,themonth,theday)",1000)
+};
+
 /**
  * Init
  */
-app.menuToggle();
-if(!Modernizr.touchevents) {
-    app.followNav();
-}
+
+app.counter(2016,01,20);
+
+setInterval(
+function () {
+    app.counter(2016,01,20)
+}, 1000 );
+
+// app.menuToggle();
+// if(!Modernizr.touchevents) {
+//     app.followNav();
+// }
